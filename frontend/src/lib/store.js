0a86f76987a708b2acad51eb94235f776b93b9e7
jaehyeok <ljhljh6887@naver.com>
2024-08-10 20:07:45 +0900

    [fix] undefined json parsing

--- frontend/src/lib/store.js
+++ frontend/src/lib/store.js
@@ -1,15 +1,30 @@
 import { writable } from "svelte/store";
 
+// const persist_storage = (key, initValue) => {
+//   // TODO: userEmail: undefined 해결
+//   const storedValueStr = localStorage.getItem(key)
+//   const store = writable(storedValueStr != null ? JSON.parse(storedValueStr) : initValue)
+//   store.subscribe((val) => {
+//     localStorage.setItem(key, JSON.stringify(val))
+//   })
+//   return store
+// }
+
 const persist_storage = (key, initValue) => {
-  const storedValueStr = localStorage.getItem(key)
-  const store = writable(storedValueStr != null ? JSON.parse(storedValueStr) : initValue)
+  const storedValueStr = localStorage.getItem(key);
+
+  // 만약 storedValueStr이 null 또는 undefined라면 initValue를 사용
+  const store = writable(storedValueStr !== null && storedValueStr !== 'undefined' ? JSON.parse(storedValueStr) : initValue);
+
   store.subscribe((val) => {
-    localStorage.setItem(key, JSON.stringify(val))
-  })
-  return store
+    localStorage.setItem(key, JSON.stringify(val));
+  });
+
+  return store;
 }
 
 export const isLogin = persist_storage("isLogin", false)
 export const userEmail = persist_storage("userEmail", "")
 export const accessToken = persist_storage("accessToken", "")
 export const isSignUpPage = persist_storage("isSignUpPage", false)
+export const chatHistoryTitles = persist_storage("chatHistoryTitles", []);

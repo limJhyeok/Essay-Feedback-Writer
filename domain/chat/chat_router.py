8382061febda325e834051ecc12f692e1cca9331
jaehyeok <ljhljh6887@naver.com>
2024-08-16 20:10:28 +0900

    [fix] user current user in backend

--- domain/chat/chat_router.py
+++ domain/chat/chat_router.py
@@ -4,22 +4,29 @@ from sqlalchemy.orm import Session
 from database import get_db
 from domain.chat import chat_crud
 from domain.chat import chat_schema
+from domain.user import user_router
+from models import User
 router = APIRouter(
     prefix = "/api/chat"
 )
 
+EMPTY_CHAT_SESSION_ID = -1
 
-@router.get("/titles/{user_id}")
-def get_chat_history_titles(user_id: int, db: Session = Depends(get_db)):
-    db_chat_titles = chat_crud.get_chat_histories(db, user_id)
+@router.get("/titles")
+def get_chat_history_titles(current_user: User = Depends(user_router.get_current_user), db: Session = Depends(get_db)):
+    db_chat_titles = chat_crud.get_chat_histories(db, current_user.id)
     if not db_chat_titles:
-        raise HTTPException(status_code=404, detail="No chat found for this user")
+        return []
     
     chat_titles = [{'id': db_chat_title.id, 'name': db_chat_title.title} for db_chat_title in db_chat_titles]
     return chat_titles
 
 @router.get("/session/{chat_id}")
 def get_chat_session_messages(chat_id: int, db: Session = Depends(get_db)):
+    # TODO: chat session이 만들어지지 않았거나 특별한 chat session을 선택하지 않았을 때 새로운 chat session을 만들어서 chatting 시작하기
+    if chat_id == EMPTY_CHAT_SESSION_ID:
+        pass
+        # create chat session
     db_chat_sessions = chat_crud.get_chat_sessions(db, chat_id)
 
     res = {"message_history": {

aad911f06c1c8a0b14700d4f9873f90a13d3598d
jaehyeok <ljhljh6887@naver.com>
2024-08-17 16:02:42 +0900

    [fix] constraint the bot and user id on chat session

--- models.py
+++ models.py
@@ -1,7 +1,8 @@
-from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, MetaData, DateTime
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, MetaData, DateTime, Enum
 from sqlalchemy.sql import func
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
+import enum
 
 Base = declarative_base()
 naming_convention = {
@@ -37,19 +38,27 @@ class Chat(Base):
     created_at = Column(DateTime, default=func.now(), nullable=False)
     updated_at = Column(DateTime, default=func.now(), onupdate=func.now(), nullable=False)
     user = relationship("User", backref="chats")
-    bot = relationship("Bot", backref="chats")
+
+class ChatSessionSenderType(enum.Enum):
+    user = "user"
+    bot = "bot"
 
 class ChatSession(Base):
     __tablename__ = "chat_session"
     id = Column(Integer, primary_key=True, index=True)
     chat_id = Column(Integer, ForeignKey("chat.id"), nullable=False) 
-    sender = Column(String, nullable=False)
+    sender = Column(Enum(ChatSessionSenderType), nullable=False)  
+    sender_id = Column(Integer, nullable=False, default = -1)
     message = Column(String, nullable=True)
     created_at = Column(DateTime, default=func.now(), nullable=False)
     updated_at = Column(DateTime, default=func.now(), onupdate=func.now(), nullable=False)
     chat = relationship("Chat", backref="sessions")
-    bot = relationship("Bot", backref="sessions")
-
+    def get_sender(self):
+        if self.sender == ChatSessionSenderType.user:
+            return self.session.query(User).get(self.sender_id)
+        elif self.sender == ChatSessionSenderType.bot:
+            return self.session.query(Bot).get(self.sender_id)
+    
 class Bot(Base):
     __tablename__ = "bot"
 

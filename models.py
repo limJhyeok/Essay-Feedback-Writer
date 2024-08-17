1a7a822fbba2e3a4f55a98e003216247b632772e
jaehyeok <ljhljh6887@naver.com>
2024-08-17 15:24:05 +0900

    [feat] create bot table and connect the tables

--- models.py
+++ models.py
@@ -23,25 +23,39 @@ class User(Base):
     social_accounts = relationship("SocialAccount", backref = "user")
 
 class SocialAccount(Base):
-  __tablename__ = "social_account"
-  id = Column(Integer, primary_key = True, index= True)
-  user_id = Column(Integer, ForeignKey("user.id"))
-  provider = Column(String, nullable = False)
-  provider_user_id = Column(String, unique=True, nullable = False)
+    __tablename__ = "social_account"
+    id = Column(Integer, primary_key = True, index= True)
+    user_id = Column(Integer, ForeignKey("user.id"))
+    provider = Column(String, nullable = False)
+    provider_user_id = Column(String, unique=True, nullable = False)
 
 class Chat(Base):
-   __tablename__ = "chat"
-   id = Column(Integer, primary_key = True, index= True)
-   user_id = Column(Integer, ForeignKey("user.id"))
-   title = Column(String, nullable = True)
-   created_at = Column(DateTime, default=func.now(), nullable=False)
-   updated_at = Column(DateTime, default=func.now(), onupdate=func.now(), nullable=False)
+    __tablename__ = "chat"
+    id = Column(Integer, primary_key = True, index= True)
+    user_id = Column(Integer, ForeignKey("user.id"))
+    title = Column(String, nullable = True)
+    created_at = Column(DateTime, default=func.now(), nullable=False)
+    updated_at = Column(DateTime, default=func.now(), onupdate=func.now(), nullable=False)
+    user = relationship("User", backref="chats")
+    bot = relationship("Bot", backref="chats")
 
 class ChatSession(Base):
     __tablename__ = "chat_session"
     id = Column(Integer, primary_key=True, index=True)
-    chat_id = Column(Integer, ForeignKey("chat.id"), nullable=False)  # 외래 키 추가
+    chat_id = Column(Integer, ForeignKey("chat.id"), nullable=False) 
     sender = Column(String, nullable=False)
     message = Column(String, nullable=True)
     created_at = Column(DateTime, default=func.now(), nullable=False)
     updated_at = Column(DateTime, default=func.now(), onupdate=func.now(), nullable=False)
+    chat = relationship("Chat", backref="sessions")
+    bot = relationship("Bot", backref="sessions")
+
+class Bot(Base):
+    __tablename__ = "bot"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)  # 예: GPT
+    version = Column(String, nullable=True)  # 예: 4.0
+    description = Column(String, nullable=True)  # AI 모델에 대한 설명
+    created_at = Column(DateTime, default=func.now(), nullable=False)  # 생성일
+    updated_at = Column(DateTime, default=func.now(), onupdate=func.now(), nullable=False)  # 업데이트일

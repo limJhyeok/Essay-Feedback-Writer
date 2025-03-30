68220e7794287c81ac538130655ef3bc79965822
limJhyeok <ljh6887@gmail.com>
2025-03-30 08:59:56 +0000

    [chore] remove unwanted letter

--- frontend/src/routes/Home.svelte
+++ frontend/src/routes/Home.svelte
@@ -714,7 +714,7 @@
   </div>
 {/if}
 
-<!-- 새 채팅 생성 modal -->`
+<!-- 새 채팅 생성 modal -->
 {#if isNewChatModalOpen}
   <div class="modal fade show d-block" tabindex="-1" style="background-color: rgba(0, 0, 0, 0.5);">
     <div class="modal-dialog modal-dialog-centered">
